d8a5740782e66e754d2dd2ad34262b69c36b7112
SaarAvraham <av.saar93@gmail.com>
2018-12-10 17:22:23 +0200

    add CellBoard class

--- JavaFX/src/GUI/StatsController.java
+++ JavaFX/src/GUI/StatsController.java
@@ -4,14 +4,17 @@ import GameEngine.Player;
 import GameEngine.eDiscType;
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.BooleanBinding;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.Callback;
 
 import java.math.BigInteger;
 import java.net.URL;
@@ -26,6 +29,7 @@ public class StatsController {
 
     @FXML private TableView<Player> tableView;
 
+    @FXML TableColumn<Player,String> turnColumn;
     @FXML TableColumn<Player,String> colorColumn;
     @FXML TableColumn<Player,String> nameColumn;
     @FXML TableColumn<Player,Integer> scoreColumn;
@@ -37,6 +41,32 @@ public class StatsController {
 //        playersList.add(new Player("Saar", true, eDiscType.WHITE, new BigInteger("1")));
 //        ObservableList<Player> playersData = FXCollections.observableArrayList(playersList);
 
+//        Bindings.equal(mainController.getGameManager().getActivePlayer());
+//        turnColumn.setCellValueFactory();
+
+//        turnColumn.setCellFactory(new Callback<TableColumn, TableCell>() {
+//            public TableCell call(TableColumn param) {
+//                return new TableCell<Player, String>() {
+//
+//                    @Override
+//                    public void updateItem(String item, boolean empty) {
+//                        super.updateItem(item, empty);
+//                        if (!isEmpty()) {
+//                            item.
+//
+////                            this.setTextFill(Color.RED);
+////                            // Get fancy and change color based on data
+////                            if(item.contains("@"))
+////                                this.setTextFill(Color.BLUEVIOLET);
+////                            setText(item);
+//                        }
+//                    }
+//                };
+//            }
+//        });
+
+        Player activePlayer = mainController.getGameManager().getActivePlayer();
+        turnColumn.setCellValueFactory(Bindings.equal());
         colorColumn.setCellValueFactory(new PropertyValueFactory<>("color"));
         nameColumn.setCellValueFactory(new PropertyValueFactory<Player, String>("name"));
         scoreColumn.setCellValueFactory(new PropertyValueFactory<Player, Integer>("score"));
@@ -63,6 +93,11 @@ public class StatsController {
 
         tableView.setFixedCellSize(25);
         tableView.prefHeightProperty().bind(Bindings.size(tableView.getItems()).multiply(tableView.getFixedCellSize()).add(26));
+    }
+
+    public void refreshTable(){
+        tableView.refresh();
+
     }
 
 //    public void highlightActivePlayer(Player activePlayer){
