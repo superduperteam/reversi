1db4af980114bb71b6a665f5555efa8453979d9e
Ido Weitzman <ido5091@gmail.com>
2018-12-10 18:35:37 +0200

    implemented PopupFactory

--- JavaFX/src/GUI/AppController.java
+++ JavaFX/src/GUI/AppController.java
@@ -6,12 +6,18 @@ import GameEngine.Point;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Popup;
+import javafx.stage.Stage;
 
 public class AppController {
     private GameManager gameManager;
     private BoardController boardController;
+    private Stage primaryStage;
     @FXML private StatsController statsComponentController;
 
+    public void setPrimaryStage(Stage _primaryStage) {primaryStage = _primaryStage;}
     public void setBoardController(BoardController boardController) {
         this.boardController = boardController;
     }
@@ -47,5 +53,29 @@ public class AppController {
             gameManager.changeTurn();
             //statsComponentController. need to highlight next player
         }
+
+        String winnerPlayerName;
+        if(gameManager.isGameOver()){
+            if(gameManager.getHighestScoringPlayers().size() == 1) {
+                winnerPlayerName = gameManager.getHighestScoringPlayers().get(0).getName();
+            } else {//it's a tie.
+                winnerPlayerName = "";
+            }
+            onGameOver(winnerPlayerName);
+        }
+    }
+
+    public void onGameOver(String winnerPlayerNAme){
+        StringBuilder winMessageBuilder = new StringBuilder();
+
+        winMessageBuilder.append("Game Over!\n");
+        if(!winnerPlayerNAme.equals("")) {
+            winMessageBuilder.append(winnerPlayerNAme)
+                    .append(" Is the winner!");
+        } else {
+            winMessageBuilder.append("It's a tie!");
+        }
+
+        PopupFactory.showPopup(winMessageBuilder.toString());
     }
 }
