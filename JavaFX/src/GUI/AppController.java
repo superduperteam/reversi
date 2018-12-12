5869fef317d11621c0538f0f7bb83d3302def973
Ido Weitzman <ido5091@gmail.com>
2018-12-12 20:38:23 +0200

    Merge branch 'javafx' of C:\Users\Ido\IdeaProjects\reversi with conflicts.

--- JavaFX/src/GUI/AppController.java
+++ JavaFX/src/GUI/AppController.java
@@ -2,7 +2,6 @@ package GUI;
 
 import GameEngine.GameManager;
 import GameEngine.Player;
-import GameEngine.GameManager.TurnHistory.Turn;
 import GameEngine.Point;
 import javafx.animation.KeyFrame;
 import javafx.animation.PauseTransition;
@@ -15,13 +14,13 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.util.Duration;
 
 import javax.swing.plaf.PopupMenuUI;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.Optional;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -31,16 +30,14 @@ public class AppController {
 
     private GameManager gameManager;
 
-    private boolean isInReplayMode;
-    private ListIterator<Turn> replayTurnIterator;
     private BoardController boardController;
     private boolean isTutorialMode = false;
     @FXML private StatsController statsComponentController;
     @FXML private CheckBox tutorialMode;
     @FXML private Button undoLastMoveButton;
-    @FXML private Button replayModeButton;
-    @FXML private Button replayModePrevButton;
-    @FXML private Button replayModeNextButton;
+    @FXML private Label taskMessageLabel;
+    @FXML private Button loadFileButton;
+    @FXML private Button startGameButton;
 
     public void setBoardController(BoardController boardController) {
         this.boardController = boardController;
@@ -69,92 +66,44 @@ public class AppController {
                 isTutorialMode = false;
             }
 
-            boardController.updateGIUDiscs(gameManager.getBoard(), isTutorialMode);
+            boardController.updateGIUDiscs(isTutorialMode);
+        });
+
+        loadFileButton.setOnMouseClicked((event) -> {
+            LoadFileTask loadFileTask = new LoadFileTask();
+            bindTaskToUIComponents(loadFileTask, ()->{});
+            new Thread(loadFileTask).start();
+        });
+
+        startGameButton.setOnMouseClicked((event)-> gameManager.activateGame());
+    }
+
+    public void bindTaskToUIComponents(Task<Boolean> aTask, Runnable onFinish) {
+        // task message
+        taskMessageLabel.textProperty().bind(aTask.messageProperty());
+
+        // task cleanup upon finish
+        aTask.valueProperty().addListener((observable, oldValue, newValue) -> {
+            onTaskFinished(Optional.ofNullable(onFinish));
         });
     }
 
-    // call this after gameManager is set.
+    public void onTaskFinished(Optional<Runnable> onFinish) {
+        taskMessageLabel.textProperty().unbind();
+        onFinish.ifPresent(Runnable::run);
+    }
+
+    // call this after gameManager is given.
     private void lateInitialize(){
         undoLastMoveButton.setOnMouseClicked(event -> { undoLastMove(); });
         undoLastMoveButton.disableProperty().bind(Bindings.not(gameManager.canUndoProperty()));
-
-        replayModeButton.setOnMouseClicked(event -> {
-            isInReplayMode = true;
-            showReplayMode();
-        });
-        replayModeButton.setDisable(true);
-
-        replayModePrevButton.setOnMouseClicked(event -> { showPrevTurn(); });
-        replayModePrevButton.setDisable(true);
-
-        replayModeNextButton.setOnMouseClicked(event -> { showNextTurn(); });
-        replayModeNextButton.setDisable(true);
-        isInReplayMode = false;
     }
-
-    private void showReplayMode() {
-        replayModeButton.setDisable(true);
-        replayModePrevButton.setDisable(false);
-
-        List<Turn> turnsList = gameManager.getHistoryOfTurns();
-        replayTurnIterator = turnsList.listIterator(turnsList.size() - 1);
-        showTurnInGIU(replayTurnIterator.next());
-        replayTurnIterator.previous();
-    }
-
-    private void showPrevTurn() {
-        if(replayTurnIterator.hasPrevious()){
-            Turn currTurnToShow = replayTurnIterator.previous();
-            showTurnInGIU(currTurnToShow);
-            replayModeNextButton.setDisable(false);
-
-            updateReplayModePrevNextButtons();
-        }
-    }
-
-    private void showNextTurn() {
-        if(replayTurnIterator.hasNext()){
-            if(replayTurnIterator.hasNext()){
-                replayTurnIterator.next();
-                Turn currTurnToShow = replayTurnIterator.next();
-                replayTurnIterator.previous();
-                showTurnInGIU(currTurnToShow);
-                replayModePrevButton.setDisable(false);
-            }
-
-
-            updateReplayModePrevNextButtons();
-        }
-    }
-
-    private void updateReplayModePrevNextButtons(){
-        replayModePrevButton.setDisable(!replayTurnIterator.hasPrevious());
-        if(replayTurnIterator.hasNext()){
-            replayTurnIterator.next();
-            replayModeNextButton.setDisable(!replayTurnIterator.hasNext());
-            replayTurnIterator.previous();
-        }
-        else{
-            replayModeNextButton.setDisable(!replayTurnIterator.hasNext());
-        }
-    }
-
-
-
-
-    private void showTurnInGIU(Turn turnToShow){
-        List<Player> turnPlayerList = turnToShow.getPlayersList();
-
-        boardController.updateGIUDiscs(turnToShow.getBoard(), false);
-        statsComponentController.refreshTable(turnPlayerList,  turnToShow.getActivePlayer());
-    }
-
 
     public void updateGUI(){
         boolean showHintsForPlayer = isTutorialMode && gameManager.getActivePlayer().isHuman();
 
-        boardController.updateGIUDiscs(gameManager.getBoard(), showHintsForPlayer);
-        statsComponentController.refreshTable(gameManager.getPlayersList(), gameManager.getActivePlayer());
+        boardController.updateGIUDiscs(showHintsForPlayer);
+        statsComponentController.refreshTable();
     }
 
     private void undoLastMove() {
@@ -167,7 +116,7 @@ public class AppController {
     }
 
     public void initTable() {
-        statsComponentController.setPlayers(gameManager.getPlayersList(), gameManager.getActivePlayer());
+        statsComponentController.setPlayers(gameManager.getPlayersList());
     }
 
     public void playTurn(Point clickedCellBoardPoint) {
@@ -202,6 +151,7 @@ public class AppController {
     }
 
     public void onGameOver(){
+        System.out.println("Test");
         StringBuilder winMessageBuilder = new StringBuilder();
 
         winMessageBuilder.append("Game Over.\n");
@@ -213,11 +163,5 @@ public class AppController {
             winMessageBuilder.append("It's a tie!");
         }
         PopupFactory.showPopup(winMessageBuilder.toString());
-
-        replayModeButton.setDisable(false);
-    }
-
-    public boolean isInReplayMode() {
-        return isInReplayMode;
     }
 }
---
+++ JavaFX/src/GUI/LoadFileTask.java
@@ -0,0 +1,113 @@
+package GUI;
+
+import Exceptions.*;
+import GameEngine.GameManager;
+import GameEngine.GameSettingsReader;
+import javafx.concurrent.Task;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class LoadFileTask extends Task<Boolean> {
+
+    private String fileName;
+    private Consumer<Runnable> onCancel;
+    private Stage primaryStage;
+    private GameManager gameManager;
+
+    @Override
+    protected Boolean call() throws Exception {
+        FileChooser fileChooser = new FileChooser();
+        String[] extensions = {".xml", ".xmL", ".xMl", ".Xml", ".xML", ".XmL", ".XMl", ".XML"};
+        boolean didLoadSuccessfully;
+
+        fileChooser.setTitle("Select XML game file");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("XML files", extensions));
+        File selectedFile = fileChooser.showOpenDialog(primaryStage);
+        if (selectedFile == null) {
+            return Boolean.FALSE;
+        }
+
+        updateMessage("Successfully fetched file");
+        String absolutePath = selectedFile.getAbsolutePath();
+        updateMessage("Loading XML file...");
+        didLoadSuccessfully = loadXML(absolutePath);
+        if(didLoadSuccessfully){
+            updateMessage("Successfully loaded XML file!");
+        }
+
+        return Boolean.TRUE;
+    }
+
+    public boolean loadXML(String filePath){
+        GameManager currGameManager;
+        GameSettingsReader gameSettingsReader = new GameSettingsReader();
+        boolean isGameLoaded = true;
+        //System.out.println("Please enter a XML path (it should end with \".xml\")");
+        //Path filePath = getFilePathFromUser();
+        //Path filePath = Paths.get("C:\\Users\\Ido\\IdeaProjects\\reversi\\GameEngine\\src\\resources\\master.xml");
+
+        if(filePath != null) {
+            try {
+                currGameManager = gameSettingsReader.readGameSettings(Paths.get(filePath));
+                if(currGameManager != null)
+                {
+                    gameManager = currGameManager;
+                }
+            } catch (FileIsNotXML fileIsNotXML){
+                updateMessage("Error: " + fileIsNotXML);
+                isGameLoaded = false;
+            } catch (NoXMLFileException noXMLFile) {
+                //noXMLFile.printStackTrace();
+                updateMessage("Error: " + noXMLFile);
+                isGameLoaded = false;
+            } catch (OutOfRangeNumberOfPlayersException playersInitPositionsOutOfRangeException) {
+                updateMessage("Error: " + playersInitPositionsOutOfRangeException);
+                isGameLoaded = false;
+            } catch (TooManyInitialPositionsException tooManyInitialPositionsException) {
+                updateMessage("Error: " + tooManyInitialPositionsException);
+                isGameLoaded = false;
+            } catch (ThereAreAtLeastTwoPlayersWithSameID thereAreAtLeastTwoPlayersWithSameID) {
+                updateMessage("Error: " + thereAreAtLeastTwoPlayersWithSameID);
+                isGameLoaded = false;
+            }
+            catch (PlayerHasNoInitialPositionsException playerHasNoInitialPositionsException) {
+                updateMessage("Error: " + playerHasNoInitialPositionsException);
+                isGameLoaded = false;
+            } catch (PlayersInitPositionsOverrideEachOtherException playersInitPositionsOverrideEachOtherException) {
+                //playersInitPositionsOverrideEachOtherException.printStackTrace();
+                updateMessage("Error: " + playersInitPositionsOverrideEachOtherException);
+                isGameLoaded = false;
+            } catch (BoardSizeDoesntMatchNumOfPlayersException boardSizeDoesntMatchNumOfPlayersException) {
+                //boardSizeDoesntMatchNumOfPlayersException.printStackTrace();
+                updateMessage("Error: " + boardSizeDoesntMatchNumOfPlayersException);
+                isGameLoaded = false;
+            } catch (PlayersInitPositionsOutOfRangeException playersInitPositionsOutOfRangeException) {
+                //playersInitPositionsOutOfRangeException.printStackTrace();
+                updateMessage("Error: " + playersInitPositionsOutOfRangeException);
+                isGameLoaded = false;
+            } catch (RowsNotInRangeException rowsNotInRangeException) {
+                //rowsNotInRangeException.printStackTrace();
+                updateMessage("Error: " + rowsNotInRangeException);
+                isGameLoaded = false;
+            } catch (ColumnsNotInRangeException columnsNotInRangeException) {
+                //columnsNotInRangeException.printStackTrace();
+                updateMessage("Error: " + columnsNotInRangeException);
+                isGameLoaded = false;
+            } catch (IslandsOnRegularModeException islandsOnRegularModeException) {
+                //islandsOnRegularModeException.printStackTrace();
+                updateMessage("Error: " + islandsOnRegularModeException);
+                isGameLoaded = false;
+            }
+        }
+        else {
+            isGameLoaded  = false;
+        }
+        return isGameLoaded;
+    }
+}
