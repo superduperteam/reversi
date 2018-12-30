14e31b8ebc90e1213c20c35b354ab4176e55366d
Ido Weitzman <ido5091@gmail.com>
2018-12-30 13:49:11 +0200

    fixed scroll in game board

--- JavaFX/src/GUI/Controllers/AppController.java
+++ JavaFX/src/GUI/Controllers/AppController.java
@@ -14,6 +14,7 @@ import javafx.beans.property.SimpleBooleanProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.util.List;
