44b7121d12e1783ac0b679cee63e759c80e6e394
Ido Weitzman <ido5091@gmail.com>
2019-02-07 16:09:00 +0200

    imporved styling

--- WebApp/src/game/servlets/lobby/LobbyServlet.java
+++ WebApp/src/game/servlets/lobby/LobbyServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
 public class LobbyServlet extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
